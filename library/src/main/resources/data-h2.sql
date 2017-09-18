d5604e3699b658d987d492b9d856dbd7d368ed4a
amrit <ashankardutta@tycoint.com>
2017-09-18 16:12:19 +0530

    changed startup script for H@ db

--- library/src/main/resources/data-h2.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO BOOK ( book_name, location) VALUES
-  (1,'Amrit', 'local'),
-  (2,'Amrit1', 'local');
---
+++ library/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO BOOK ( book_name, location) VALUES('SpringBoot', 'local');
+INSERT INTO BOOK ( book_name, location) VALUES('Zuul', 'local');
