4862781a6d9282615a6adee836c6b22a9240f9b3
amrit <ashankardutta@tycoint.com>
2017-09-18 13:10:44 +0530

    adding config server

--- library/src/main/java/com/example/library/LibraryApplication.java
+++ library/src/main/java/com/example/library/LibraryApplication.java
@@ -1,13 +1,16 @@
 package com.example.library;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -32,5 +35,18 @@ class ServiceInstanceRestController {
     public List<ServiceInstance> serviceInstancesByApplicationName(
             @PathVariable String applicationName) {
         return this.discoveryClient.getInstances(applicationName);
+    }
+}
+
+@RestController
+@RefreshScope
+class ExampleController {
+
+    @Value("${message}")
+    private String value;
+
+    @RequestMapping("/home")
+    public @ResponseBody String sayValue() {
+        return value;
     }
 }
